6e9c4e8a58f06187d29f23d072168315558e9f3a
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 23:48:51 +0500

    Task6

---
+++ Tyuiu.KruchininEP.Sprint6.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KruchininEP.Sprint6.Task6.V8.Lib;
+namespace Tyuiu.KruchininEP.Sprint6.Task6.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = "1";
+
+            var res = ds.CollectTextFromFile(path);
+            string wait = "1";
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
---
+++ Tyuiu.KruchininEP.Sprint6.Task6.V8/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KruchininEP.Sprint6.Task6.V8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
