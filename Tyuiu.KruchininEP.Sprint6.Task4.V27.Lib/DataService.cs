7b35df22b77a75928400529a243111bf33a079c1
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 22:31:18 +0500

    Task4

---
+++ Tyuiu.KruchininEP.Sprint6.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Tyuiu.KruchininEP.Sprint6.Task4.V27.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.KruchininEP.Sprint6.Task4.V27/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KruchininEP.Sprint6.Task4.V27
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
