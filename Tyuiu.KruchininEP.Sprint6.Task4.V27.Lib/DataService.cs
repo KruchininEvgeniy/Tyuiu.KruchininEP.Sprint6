ebe8b347df8cdb35fcfaff2386ab6d9e61cfec0a
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 22:32:59 +0500

    Task4

--- Tyuiu.KruchininEP.Sprint6.Task4.V27.Lib/DataService.cs
+++ Tyuiu.KruchininEP.Sprint6.Task4.V27.Lib/DataService.cs
@@ -1,8 +1,27 @@
-﻿
-namespace Tyuiu.KruchininEP.Sprint6.Task4.V27.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.KornevRM.Sprint6.Task4.V27.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task4V27
     {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] result = new double[(stopValue - startValue) + 1];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == -1)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = Math.Round((Math.Cos(x) / (x + 1) - Math.Cos(x) * 1.3 + 3 * x), 2);
+                }
 
+                result[count++] = y;
+            }
+            return result;
+        }
     }
 }
