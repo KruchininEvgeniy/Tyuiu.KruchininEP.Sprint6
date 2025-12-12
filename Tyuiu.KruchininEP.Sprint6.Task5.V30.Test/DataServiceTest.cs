d2a108a9a4529c7d142c44f809a49d2db93acd79
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 22:41:38 +0500

    Task5

---
+++ Tyuiu.KruchininEP.Sprint6.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KruchininEP.Sprint6.Task5.V30.Lib;
+using System.IO;
+
+namespace Tyuiu.NovikovD.Sprint6.Task5.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint6\Tyuiu.KruchininEP.Sprint6.Task5.V30.Test\bin\Debug\net8.0InPutFileTask5V30.txt";
+
+            DataService ds = new DataService();
+            double[] res = ds.LoadFromDataFile(path);
+            double[] wait = { 5 };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KruchininEP.Sprint6.Task5.V30/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KruchininEP.Sprint6.Task5.V30
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
