edb7b4f9470cf0eb075aa94f3b3dd4cd2e181c61
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 22:25:16 +0500

    Task3

---
+++ Tyuiu.KruchininEP.Sprint6.Task3.V30.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.KruchininEP.Sprint6.Task3.V30.Lib
+{
+    public class DataService : ISprint6Task3V30
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == 4 && matrix[i, j] % 2 == 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.KruchininEP.Sprint6.Task3.V30/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KruchininEP.Sprint6.Task3.V30
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
