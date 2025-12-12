55f132d2338f11bcfac84f9ab5d9d95478a0b6bb
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-12-12 22:12:08 +0500

    Task1

---
+++ Tyuiu.KruchininEP.Sprint6.Task1.V17/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.KruchininEP.Sprint6.Task1.V17
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
